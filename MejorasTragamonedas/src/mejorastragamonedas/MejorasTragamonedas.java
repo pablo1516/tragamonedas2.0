0953d254059adaf525882889902cb98b219bea78
pablo1516 <61156436+pablo1516@users.noreply.github.com>
2020-03-25 22:23:25 -0500

    Add files via upload

---
+++ MejorasTragamonedas/src/mejorastragamonedas/MejorasTragamonedas.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mejorastragamonedas;
+
+/**
+ *
+ * @author david
+ */
+public class MejorasTragamonedas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrmTragamonedas frmTragamonedas = new FrmTragamonedas();
+        frmTragamonedas.setVisible(true);
+    }
+    
+}
